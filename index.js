9e4ef62529d83a06d0d7b7403156448e50881bdb
Bruno Santos <bruno_st17@hotmail.com>
2022-02-01 11:48:53 -0300

    Ajustando index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 const port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
+app.get("/", function(req, res){
     res.send("Olá, realizando teste com Heroku nesse endpoint")
 })
 
